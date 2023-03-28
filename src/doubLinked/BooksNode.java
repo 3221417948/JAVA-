c8138e419ca1c80da6fe762ca9d9f06127461a95
Noah <3221417948@qq.com>
2023-03-28 22:08:29 +0800

    双链表

--- src/doubLinked/BooksNode.java
+++ src/doubLinked/BooksNode.java
@@ -2,16 +2,16 @@ package doubLinked;
 
 //图书结点类（双链表）
 public class BooksNode {
-    public int bid; //图书编号
-    public String bname; //图书名称
+    public int bId; //图书编号
+    public String bName; //图书名称
     public double bPrice; //图书价格
 
     public BooksNode next; //指针指向下一个结点
     public BooksNode front;//指针指向上一个节点
 
-    public BooksNode(int bid, String bname, double bPrice) {
-        this.bid = bid;
-        this.bname = bname;
+    public BooksNode(int bId, String bName, double bPrice) {
+        this.bId = bId;
+        this.bName = bName;
         this.bPrice = bPrice;
     }
 }
